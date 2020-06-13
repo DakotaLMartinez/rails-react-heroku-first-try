9dc110f7f3b37087977921b311cc863581a70335
github username <github email address>
2020-06-13 12:01:03 -0700

    add posts resource and api index endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do 
+    namespace :v1 do 
+      resources :posts
+    end 
+  end
   devise_for :users
   get 'welcome/home'
   get 'welcome/app', to: 'welcome#app', as: 'app'
